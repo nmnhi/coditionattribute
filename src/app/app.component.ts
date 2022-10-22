b0cb6735a02f88382a058b832e79c4d114f4194b
Nhi Nguyen <minhnhi08061105@gmail.com>
2022-10-22 21:35:54 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'conditionattribute';
+  videos = [
+    {
+      title: 'My video 1',
+      share: 415,
+      likes: 257,
+      dislikes: 12,
+      thumnail: '../assets/images/image1.jpg',
+    },
+    {
+      title: 'My video 2',
+      share: 215,
+      likes: 207,
+      dislikes: 12,
+      thumnail: '../assets/images/image2.jpg',
+    },
+    {
+      title: 'My video 3',
+      share: 150,
+      likes: 109,
+      dislikes: 12,
+      thumnail: '../assets/images/image3.jpeg',
+    },
+  ];
+  mostliekedVideo = this.getmostlikedVideo();
+  getmostlikedVideo() {
+    let videoCopy = [...this.videos];
+    return videoCopy.sort((curr, next) => next.likes - curr.likes)[0];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
